72163bfd17df865ef84677af6bd8e09b036fb739
Leonard <leonardposner@googlemail.com>
2017-01-11 21:46:28 +0100

    ScoreTable added

---
+++ src/data/ScoreTable.java
@@ -0,0 +1,25 @@
+package data;
+
+/**Substitutions Matrix for sequenz alignment of proteins
+ * Created by leonard on 11.01.2017.
+ */
+public class ScoreTable {
+
+     double matrix [][];
+
+
+    /**
+     * Constructor.
+     *
+     * @param dateipfad
+     */
+        public ScoreTable(String dateipfad){
+
+        }
+
+
+
+
+
+
+}
