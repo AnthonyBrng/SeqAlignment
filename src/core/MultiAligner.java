2e9ce895b33031f37e75984050fc59668a2fbd0b
anthony <anthony.bruening@gmail.com>
2017-02-07 19:24:56 +0100

    MultiAligner

---
+++ src/core/MultiAligner.java
@@ -0,0 +1,29 @@
+package core;
+
+import structures.Record;
+
+/**
+ * Created by anthony on 07.02.17.
+ */
+public class MultiAligner extends Aligner
+{
+
+
+    @Override
+    public Record cellValue(int x, int y)
+    {
+        return null;
+    }
+
+    @Override
+    public void findTraceBackStart()
+    {
+
+    }
+
+    @Override
+    public void initTable()
+    {
+
+    }
+}
